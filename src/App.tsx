293d436ddef10d5c7e6c4bd460a7f8b88b05ad42
SaoodCS <saood-aslam@outlook.com>
2023-09-17 13:12:49 +0100

    adding react query dev tools

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { useContext } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { ThemeContext } from './global/context/theme/ThemeContext';
 import { GlobalTheme } from './global/styles/theme';
+import ErrorPage from './pages/errors/ErrorPage';
+import NotFound from './pages/errors/NotFound';
+
+const queryClient = new QueryClient();
 
 export default function App(): JSX.Element {
    const { isDarkTheme, toggleTheme } = useContext(ThemeContext);
@@ -8,7 +15,20 @@ export default function App(): JSX.Element {
    return (
       <>
          <GlobalTheme darkTheme={isDarkTheme} />
-         Hello
+         <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+               <Routes>
+                  <Route path="*" element={<NotFound />} key={'notFound'} />
+                  <Route
+                     path="/"
+                     element={<div>Landing</div>}
+                     key={'landing'}
+                     errorElement={<ErrorPage />}
+                  ></Route>
+               </Routes>
+            </BrowserRouter>
+            <ReactQueryDevtools initialIsOpen={false} />
+         </QueryClientProvider>
       </>
    );
 }
