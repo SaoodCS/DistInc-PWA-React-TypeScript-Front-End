451c808a2b0b72da44915006515d84055ab831f8
SaoodCS <saood-aslam@outlook.com>
2023-09-17 21:00:40 +0100

    cleaning code

--- src/main.tsx
+++ src/main.tsx
@@ -13,12 +13,10 @@ function Root(): JSX.Element {
          <ThemeContextProvider>
             <AuthContextProvider>
                <WidgetContextProviders>
-                  <>
                      <InstallAppModal />
                      {/* <EnablePushNotifModal /> */}
                      <UpdateAppModal />
                      <App />
-                  </>
                </WidgetContextProviders>
             </AuthContextProvider>
          </ThemeContextProvider>
