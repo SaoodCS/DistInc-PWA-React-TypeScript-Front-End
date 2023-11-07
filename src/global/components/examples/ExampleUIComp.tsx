f8aa1bed8535e88b5f8d628a793416f3f13372e4
SaoodCS <saood-aslam@outlook.com>
2023-11-07 17:01:04 +0000

    linting fixes

--- src/global/components/examples/ExampleUIComp.tsx
+++ src/global/components/examples/ExampleUIComp.tsx
@@ -2,7 +2,6 @@ import BannerExample from './components/BannerExample';
 import BottomPanelExample from './components/BottomPanelExample';
 import CarouselExample from './components/CarouselExample';
 import CarouselWithScrollSaverExample from './components/CarouselWithScrollSaverExample';
-import ContextMenuExample from './components/ContextMenuExample';
 import LoaderExample from './components/LoaderExample';
 import ModalExample from './components/ModalExample';
 import PopupMenuExample from './components/PopupMenuExample';
@@ -20,6 +19,5 @@ export default class ExampleUIComp {
    static Toast = ToastExample;
    static ScrollSaver = ScrollSaverExample;
    static PushNotif = PushNotifExample;
-   static ContextMenu = ContextMenuExample;
    static PopupMenu = PopupMenuExample;
 }
