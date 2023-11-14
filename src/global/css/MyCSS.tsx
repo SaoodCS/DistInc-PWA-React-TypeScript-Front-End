c0b9fa18f3dda6f8920078dc3cdc0ba9aac76fab
SaoodCS <saood-aslam@outlook.com>
2023-11-14 13:30:59 +0000

    cleaned up css styles

--- src/global/css/MyCSS.tsx
+++ src/global/css/MyCSS.tsx
@@ -16,34 +16,22 @@ export namespace MyCSS {
          -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
          -webkit-touch-callout: none;
       `;
+      
       export const portable = {
          changeColorOnClick: (
             changeToColor: string,
-            propertyToChange: 'background-color' | 'color' | 'border',
+            property: 'background-color' | 'color' | 'border',
             postClick: 'revert' | 'persist',
          ) => {
+            const selector = postClick === 'persist' ? '&:active' : '&:hover';
+            const propVal = property === 'border' ? `1px solid ${changeToColor}` : changeToColor;
             return css`
                @media (max-width: ${MyCSS.PortableBp.asPx}) {
-                  transition: ${propertyToChange} 0.2s;
-                  ${postClick === 'persist'
-                     ? `&:hover {
-                        ${
-                           propertyToChange === 'background-color'
-                              ? `background-color: ${changeToColor};`
-                              : propertyToChange === 'color'
-                              ? `color: ${changeToColor};`
-                              : `border: 1px solid ${changeToColor};`
-                        }
-                     }`
-                     : `&:active {
-                        ${
-                           propertyToChange === 'background-color'
-                              ? `background-color: ${changeToColor};`
-                              : propertyToChange === 'color'
-                              ? `color: ${changeToColor};`
-                              : `border: 1px solid ${changeToColor};`
-                        }
-                     }`}
+                  cursor: pointer;
+                  transition: ${property} 0.2s;
+                  ${selector} {
+                     ${`${property}: ${propVal};`}
+                  }
                }
             `;
          },
@@ -52,50 +40,33 @@ export namespace MyCSS {
       export const desktop = {
          changeColorOnClick: (
             changeToColor: string,
-            propertyToChange: 'background-color' | 'color' | 'border',
+            property: 'background-color' | 'color' | 'border',
             postClick: 'revert' | 'persist',
          ) => {
+            const selector = postClick === 'revert' ? '&:active' : '&:active, &:focus'; // the else part doesn't work
+            const propVal = property === 'border' ? `1px solid ${changeToColor}` : changeToColor;
             return css`
                @media (min-width: ${MyCSS.PortableBp.asPx}) {
                   cursor: pointer;
-                  transition: ${propertyToChange} 0.2s;
-                  ${postClick === 'revert'
-                     ? `&:active {
-                        ${
-                           propertyToChange === 'background-color'
-                              ? `background-color: ${changeToColor};`
-                              : propertyToChange === 'color'
-                              ? `color: ${changeToColor};`
-                              : `border: 1px solid ${changeToColor};`
-                        }
-                     }`
-                     : // this else part doesn't work
-                       `&:active, &:focus {
-                        ${
-                           propertyToChange === 'background-color'
-                              ? `background-color: ${changeToColor};`
-                              : propertyToChange === 'color'
-                              ? `color: ${changeToColor};`
-                              : `border: 1px solid ${changeToColor};`
-                        }
-                     }`}
+                  transition: ${property} 0.2s;
+                  ${selector} {
+                     ${`${property}: ${propVal};`}
+                  }
                }
             `;
          },
          changeColorOnHover: (
             changeToColor: string,
-            propertyToChange: 'background-color' | 'color' | 'border',
+            property: 'background-color' | 'color' | 'border',
          ) => {
+            const selector = '&:hover';
+            const propVal = property === 'border' ? `1px solid ${changeToColor}` : changeToColor;
             return css`
-               cursor: pointer;
-               transition: ${propertyToChange} 0.2s;
                @media (min-width: ${MyCSS.PortableBp.asPx}) {
-                  &:hover {
-                     ${propertyToChange === 'background-color'
-                        ? `background-color: ${changeToColor};`
-                        : propertyToChange === 'color'
-                        ? `color: ${changeToColor};`
-                        : `border: 1px solid ${changeToColor};`}
+                  cursor: pointer;
+                  transition: ${property} 0.2s;
+                  ${selector} {
+                     ${`${property}: ${propVal};`}
                   }
                }
             `;
