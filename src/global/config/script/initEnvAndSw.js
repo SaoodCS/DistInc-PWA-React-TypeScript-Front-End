2729756e871d39c2d49967fa8d83e43cefc821c9
SaoodCS <saood-aslam@outlook.com>
2023-12-03 13:51:59 +0000

    update initEnvAndSw file bug fixes

--- src/global/config/script/initEnvAndSw.js
+++ src/global/config/script/initEnvAndSw.js
@@ -4,7 +4,23 @@ import fs from 'fs-extra';
 const envFileName = process.argv[2];
 const runningEnvValue = process.argv[3];
 
-// Set The VITE_RUNNING env variable to the envValue (val is "locally" or "deployed"):
+// Load the .env variables and create the firebase-messaging-sw.js file:
+dotenv.config({ path: envFileName });
+fs.removeSync('public/firebase-messaging-sw.js');
+console.log('Old firebase-messaging-sw.js file deleted...');
+const config = fs.readFileSync('src/global/firebase/sw/fcmServiceWorker.js', 'utf8');
+const newConfig = config
+   .replace(/apiKey: .*,/, `apiKey: '${process.env.VITE_APIKEY}',`)
+   .replace(/authDomain: .*,/, `authDomain: '${process.env.VITE_AUTHDOMAIN}',`)
+   .replace(/projectId: .*,/, `projectId: '${process.env.VITE_PROJECTID}',`)
+   .replace(/storageBucket: .*,/, `storageBucket: '${process.env.VITE_STORAGEBUCKET}',`)
+   .replace(/messagingSenderId: .*,/, `messagingSenderId: '${process.env.VITE_MESSAGINGSENDERID}',`)
+   .replace(/appId: .*,/, `appId: '${process.env.VITE_APPID}',`)
+   .replace(/measurementId: .*,/, `measurementId: '${process.env.VITE_MEASUREMENTID}',`);
+fs.writeFileSync('public/firebase-messaging-sw.js', newConfig, 'utf8');
+console.log('New firebase-messaging-sw.js file created...');
+
+// Set The VITE_RUNNING env variable to the runningEnvValue:
 let envFile = fs.readFileSync(envFileName, 'utf8');
 envFile = envFile.replace(/^VITE_RUNNING=locally.*$/gm, '');
 envFile = envFile.replace(/^VITE_RUNNING=deployed.*$/gm, '');
@@ -16,18 +32,3 @@ console.log('VITE_RUNNING env variable updated...');
 // Remove the old "dev-dist" folder from the root folder:
 fs.removeSync('./dev-dist');
 console.log('Old dev-dist folder deleted...');
-
-// Load the env variables into fcmServiceWorker.js and copy it to the public folder as firebase-messaging-sw.js:
-dotenv.config({ path: envFileName });
-fs.removeSync('public/firebase-messaging-sw.js');
-const config = fs.readFileSync('src/global/firebase/sw/fcmServiceWorker.js', 'utf8');
-const newConfig = config
-   .replace(/apiKey: .*,/, `apiKey: '${process.env.VITE_APIKEY}',`)
-   .replace(/authDomain: .*,/, `authDomain: '${process.env.VITE_AUTHDOMAIN}',`)
-   .replace(/projectId: .*,/, `projectId: '${process.env.VITE_PROJECTID}',`)
-   .replace(/storageBucket: .*,/, `storageBucket: '${process.env.VITE_STORAGEBUCKET}',`)
-   .replace(/messagingSenderId: .*,/, `messagingSenderId: '${process.env.VITE_MESSAGINGSENDERID}',`)
-   .replace(/appId: .*,/, `appId: '${process.env.VITE_APPID}',`)
-   .replace(/measurementId: .*,/, `measurementId: '${process.env.VITE_MEASUREMENTID}',`);
-fs.writeFileSync('public/firebase-messaging-sw.js', newConfig, 'utf8');
-console.log('Fcm service worker file initialized...');
