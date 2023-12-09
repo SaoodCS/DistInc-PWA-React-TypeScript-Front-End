c9ac72bf34d576779ff70f39e87cace66437ac23
SaoodCS <saood-aslam@outlook.com>
2023-12-09 20:32:53 +0000

    Update toggleContextWidg function calls in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-// TODO: update any other toggleContextWidg() functions to be togglethContextWidg(true) or togglethContextWidg(false) like i did with the bottom panel and modal context providers
 import AppRouter from './routes/AppRouter';
 
 export default function App(): JSX.Element {
